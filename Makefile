cb8aeeca787246591f7de9060ec7af3770bcecf2
zak <zak@debian>
2017-08-28 12:58:05 -0400

    makefile and cleaning

---
+++ Makefile
@@ -0,0 +1,4 @@
+all: loto.c
+	gcc -o loto loto.c
+clean:
+	rm loto
--- loto.c
+++ loto.c
@@ -3,15 +3,14 @@
 #include <unistd.h>
 #include <time.h>
 
-void	give_sorted_grid(int *grid, size_t n)
+void		give_sorted_grid(int *grid, size_t n)
 {
-	size_t i;
+	size_t	i;
 
-	printf("grid size = %zu\n", n);
+	//printf("grid size = %zu\n", n);
 	i = 0;
 	while (i < n)
 	{
-		//write(1, "***-->debug\n", 12);
 		grid[i] = i + 1;
 		i++;
 	}
@@ -20,49 +19,65 @@ void	give_sorted_grid(int *grid, size_t n)
 
 }
 
-void	print_grid(int *grid, size_t n)
+void		print_grid(int *grid, size_t n)
 {
-	size_t i;
+	size_t	i;
 
 	printf("---------------- GRID ------------------\n");
 	for (i = 0; i < n; i++) {
-		//write(1, "loop\n", 5);
 		printf("-> Grille %d: |%d %d %d %d %d|\n", i + 1, grid[i * 5], grid[i * 5 + 1], grid[i * 5 + 2], grid[i * 5 + 3], grid[i * 5 + 4]);
 	}
 	printf("----------------------------------------\n");
 }
 
-void	print_grid_star(int *grid, size_t n)
+void		print_grid_star(int *grid, size_t n)
 {
-	size_t i;
+	size_t	i;
 
 	printf("------------- GRID STAR ----------------\n");
 	for (i = 0; i < n; i++) {
-		//write(1, "loop\n", 5);
 		printf("-> Grille %d: |%d|\n", i + 1, grid[i]);
 	}
 	printf("----------------------------------------\n");
 }
 
-void	print_paper(int *grid, int *grid_star, size_t grid_size, size_t grid_star_size)
+void		print_paper(int *grid, int *grid_star, size_t grid_size, size_t grid_star_size)
 {
 	print_grid(grid, grid_size);
 	print_grid_star(grid_star, grid_star_size);
 }
 
-int main(int ac, char *av[])
+void		shuffle_grid(int *grid, size_t size)
 {
-	int *grid;
-	int	*grid_star;
+	size_t	i;
+	int		tmp;
+	int		random_index;
 
-	if (ac == 2)
+	tmp = 0;
+	i = 0;
+	while (i < size)
 	{
-		if (!(grid = (int *)malloc(sizeof(int) * atoi(av[1]) * 5 * 49)))
+		tmp = grid[i];
+		random_index = rand() % size;
+		grid[i] = grid[random_index];
+		grid[random_index] = tmp;
+		i++;
+	}
+}
+
+int			main(int ac, char *av[])
+{
+	int		*grid;
+	int		*grid_star;
+
+	if (ac == 1)
+	{
+		if (!(grid = (int *)malloc(sizeof(int) * 5 * 49)))
 		{
 			printf("Malloc grid failed. Exit\n");
 			return (0);
 		}
-		else if (!(grid_star = (int *)malloc(sizeof(int) * atoi(av[1]) * 5 * 10)))
+		else if (!(grid_star = (int *)malloc(sizeof(int) * 5 * 10)))
 		{
 			printf("Malloc grid_start failed. Exit\n");
 			return (0);
@@ -71,10 +86,11 @@ int main(int ac, char *av[])
 		{
 			give_sorted_grid(grid, 49);
 			give_sorted_grid(grid_star, 10);
-			//generate_rand_array(grid, (size_t)atoi(av[1]));
-			write(1, "-->debug grid before\n", 22);
-			print_paper(grid, grid_star, 10, 10);
-			write(1, "\n\n-->debug grid after\n", 22);
+			//write(1, "-->debug grid before\n", 22);
+			//print_paper(grid, grid_star, 10, 10);
+			shuffle_grid(grid, 49);
+			shuffle_grid(grid_star, 10);
+			//write(1, "\n\n-->debug grid after\n", 22);
 			print_paper(grid, grid_star, 10, 10);
 		}
 		free(grid);
@@ -82,7 +98,7 @@ int main(int ac, char *av[])
 	}
 	else
 	{
-		printf("<Usage: ./loto NB_GRILLES>\n");
+		printf("<Usage: ./loto>\n");
 		return (0);
 	}
 	return (0);
